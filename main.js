13685888c0adfa089c140bc545537b227852e544
Web-Developter <84792687+Web-Developter@users.noreply.github.com>
2023-01-24 19:01:49 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,96 +1,99 @@
-song = "";
-scoreleftWrist = 0;
-scorerightWrist = 0;
+song1 = "";
+song2 = "";
 
-function preload(){
-    song = loadSound("music.mp3");
+song1_status = "";
+song2_status = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+	song1 = loadSound("I Want It That Way.mp3");
+	song2 = loadSound("Heat Waves.mp3");
 }
 
-function setup(){
-    canvas = createCanvas(600, 500);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.hide();
-    posenet = ml5.poseNet(video, modelLoaded);
-    posenet.on('pose', gotPoses);
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
-function draw(){
-    image(video, 0, 0, 600, 500);
-
-    fill('#FF0000');
-    stroke('#FF0000');
-
-    if(scorerightWrist > 0.2){
-        circle(rightWristX, rightWristY, 20);
-
-        if(rightWristY > 0 && rightWristY <= 100){
-            document.getElementById("speed").innerHTML = "Speed = 0.5x";
-            song.rate(0.5);
-        }
-
-        else if(rightWristY > 100 && rightWristY <= 200){
-            document.getElementById("speed").innerHTML = "Speed = 1x";
-            song.rate(1);
-        }
-
-        else if(rightWristY > 200 && rightWristY <= 300){
-            document.getElementById("speed").innerHTML = "Speed = 1.5x";
-            song.rate(1.5);
-        }
-
-        else if(rightWristY > 300 && rightWristY <= 400){
-            document.getElementById("speed").innerHTML = "Speed = 2x";
-            song.rate(2);
-        }
-
-        else if(rightWristY > 400 && rightWristY <= 500){
-            document.getElementById("speed").innerHTML = "Speed = 2.5x";
-            song.rate(2.5);
-        }
-    }
-
-    if(scoreleftWrist > 0.2){
-        circle(leftWristX, leftWristY, 20);
-        InNumberleftWristY = Number(leftWristY);
-        remove_decimals = floor(InNumberleftWristY);
-        volume = remove_decimals/500;
-        document.getElementById("volume").innerHTML = "Volume = " + volume;
-        song.setVolume(volume);
-    }
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
 }
 
-function play(){
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
 }
 
-leftWristX = "";
-leftWristY = "";
-rightWristX = "";
-rightWristY = "";
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
 
-function modelLoaded(){
-    console.log('posenet is initialized');
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = "Playing - I Want It That Way"
+		}
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Playing - Heat Waves"
+		}
+	}
+
 }
 
-function gotPoses(results){
-    if(results.length > 0){
-        console.log(results);
-        scoreleftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreleftWrist = " + scoreleftWrist);
-
-        scorerightWrist = results[0].pose.keypoints[10].score;
-        console.log("scorerightWrist = " + scorerightWrist);
-
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
-
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
-    }
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
 }
-
